87452f468556973c11d94da63a0c3220491bc29b
arocho <nick@boldlyfine.com>
2020-12-28 18:49:31 -0800

    Refactor models to use common Entity schema

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,46 +1,42 @@
 import { IsEmail, Length } from "class-validator";
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm";
+import {
+  Entity as TOEntity,
+  Column,
+  Index,
+  BeforeInsert,
+  OneToMany
+} from "typeorm";
 import bcrypt from "bcrypt";
-import { classToPlain, Exclude } from "class-transformer";
+import { Exclude } from "class-transformer";
 
-@Entity('users')
-export class User extends BaseEntity {
-    constructor(user: Partial<User>) {
-        super()
-        Object.assign(this, user)
-    }
+import Entity from "./Entity";
+import Post from "./Post";
+@TOEntity('users')
+export default class User extends Entity {
+  constructor(user: Partial<User>) {
+    super()
+    Object.assign(this, user)
+  }
+  @Index()
+  @IsEmail()
+  @Column({ unique: true })
+  email: string
+  
+  @Index()
+  @Length(3, 255, { message: 'Username must be at least 3 characters long' })
+  @Column({ unique: true })
+  username: string
+  
+  @Exclude()
+  @Column()
+  @Length(6, 255)
+  password: string
 
-    @Exclude()
-    @PrimaryGeneratedColumn()
-    id: number
+  @OneToMany(() => Post, post => post.user)
+    posts: Post[];
 
-    @Index()
-    @IsEmail()
-    @Column({ unique: true })
-    email: string
-    
-    @Index()
-    @Length(3, 255, { message: 'Username must be at least 3 characters long' })
-    @Column({ unique: true })
-    username: string
-    
-    @Exclude()
-    @Column()
-    @Length(6, 255)
-    password: string
-
-    @CreateDateColumn()
-    createdAt: Date
-
-    @UpdateDateColumn()
-    updatedAt: Date
-
-    @BeforeInsert()
-    async hashPassword() {
-        this.password = await bcrypt.hash(this.password, 6)
-    }
-
-    toJSON() {
-        return classToPlain(this)
-    }
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 6)
+  }
 }
