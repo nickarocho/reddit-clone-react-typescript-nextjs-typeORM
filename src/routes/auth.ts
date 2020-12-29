bb9a810c3ec37cd59b7a49b32513364585dfc30c
arocho <nick@boldlyfine.com>
2020-12-29 13:44:40 -0800

    Update tsconfig and fix probs it exposed

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,7 +8,7 @@ import User from "../entities/User";
 import auth from "../middleware/auth";
 
 const register = async (req: Request, res: Response) => {
-  const {email, username, password} = req.body;
+  const { email, username, password } = req.body;
 
   try {
     // Validate data
@@ -62,7 +62,7 @@ const login = async (req: Request, res: Response) => {
       return res.status(401).json({ password: 'Password is incorrect' });
     }
 
-    const token = jwt.sign({ username }, process.env.JWT_SECRET );
+    const token = jwt.sign({ username }, process.env.JWT_SECRET! );
     // set is used to set headers when you send back a response
     res.set(
       'Set-Cookie',
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -21,7 +21,7 @@ const createPost = async (req: Request, res: Response) => {
     return res.json(post);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: 'Something went wrong' });
+    return res.status(500).json({ error: 'Something went wrong' });
   }
 }
 
