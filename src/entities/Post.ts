cf2d36ebe3e16308a3757b47207ba97be294846e
arocho <nick@boldlyfine.com>
2020-12-28 18:50:33 -0800

    Create Post model, add Create Post functionality

---
+++ src/entities/Post.ts
@@ -0,0 +1,47 @@
+import {
+  Entity as TOEntity,
+  Column,
+  Index,
+  ManyToOne,
+  JoinColumn,
+  BeforeInsert
+} from "typeorm";
+import { makeId, slugify } from "../util/helpers";
+
+import Entity from "./Entity";
+import User from "./User";
+
+@TOEntity('posts')
+export default class Post extends Entity {
+  constructor(post: Partial<Post>) {
+    super()
+    Object.assign(this, post)
+  }
+
+  @Index()
+  @Column()
+  identifier: string // 7-character ID
+  
+  @Column()
+  title: string
+  
+  @Index()
+  @Column()
+  slug: string
+
+  @Column({ nullable: true, type: 'text' })
+  body: string
+
+  @Column()
+  subName: string
+
+  @ManyToOne(() => User, user => user.posts)
+  @JoinColumn({ name: 'username', referencedColumnName: 'username' })
+  user: User;
+
+  @BeforeInsert()
+  makeIdAndSlug() {
+    this.identifier = makeId(7);
+    this.slug = slugify(this.title);
+  }
+}
