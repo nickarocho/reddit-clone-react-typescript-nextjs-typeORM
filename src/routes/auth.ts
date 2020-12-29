ee9db941d5f75a812836f1224930adf6560c3f83
arocho <nick@boldlyfine.com>
2020-12-28 17:37:44 -0800

    Finish authentication.

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,11 @@
 import { Request, Response, Router } from "express";
 import { isEmpty, validate } from "class-validator";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
 import { User } from "../entities/User";
+import auth from "../middleware/auth";
 
 const register = async (req: Request, res: Response) => {
   const {email, username, password} = req.body;
@@ -37,7 +40,7 @@ const register = async (req: Request, res: Response) => {
 }
 
 const login = async (req: Request, res: Response) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
 
   try {
     let errors: any = {};
@@ -59,15 +62,46 @@ const login = async (req: Request, res: Response) => {
       return res.status(401).json({ password: 'Password is incorrect' });
     }
 
+    const token = jwt.sign({ username }, process.env.JWT_SECRET );
+    // set is used to set headers when you send back a response
+    res.set(
+      'Set-Cookie',
+      cookie.serialize('token', token, {
+        httpOnly: true, // means this cookie can't be accessed by Javascript - makes it more secure
+        secure: process.env.NODE_ENV === 'production', // HTTPS only in production env only
+        sameSite: 'strict', // cookie can ONLY come from our domain
+        maxAge: 3600,
+        path: '/',
+      })
+    );
+
     return res.json(user);
   } catch (err) {
-    
+    return res.status(401).json({ error: err });
   }
+}
+
+const me = (_: Request, res: Response) => {
+  return res.json(res.locals.user);
+}
+
+const logout = (_: Request, res: Response) => {
+  res.set('Set-Cookie', cookie.serialize('token', '', {
+    httpOnly: true, // means this cookie can't be accessed by Javascript - makes it more secure
+    secure: process.env.NODE_ENV === 'production', // HTTPS only in production env only
+    sameSite: 'strict', // cookie can ONLY come from our domain
+    expires: new Date(0),
+    path: '/',
+  }));
+
+  return res.status(200).json({ success: true });
 }
 
 
 const router = Router();
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', auth, me);
+router.get('/logout', auth, logout);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
 
 import authRoutes from './routes/auth';
 import trim from './middleware/trim';
@@ -11,6 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev')); // nice tool for logging server responses
 app.use(trim);
+app.use(cookieParser());
 
 app.get('/', (_, res) => res.send('Hello world!'));
 app.use('/api/auth', authRoutes);
