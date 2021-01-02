748f06430c09acf86f2f041a98054be9e80512b4
arocho <nick@boldlyfine.com>
2021-01-02 15:54:29 -0800

    Create the NavBar and add to the home page

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -15,8 +15,9 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600&display=swap"
             rel="stylesheet"
           />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" crossOrigin="anonymous" />
         </Head>
-        <body className="font-body">
+        <body className="font-body" style={{ backgroundColor: '#DAE0E6' }}>
           <Main />
           <NextScript />
         </body>
