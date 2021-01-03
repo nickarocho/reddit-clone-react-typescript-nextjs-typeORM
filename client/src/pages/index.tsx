6177cff340c4fc0cb211685573c6facc2f7e0e8d
arocho <nick@boldlyfine.com>
2021-01-03 12:13:29 -0800

    Refactor home page to use SSR for fetching posts

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,19 +6,20 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 
 import { Post } from '../types'
+import { GetServerSideProps } from 'next';
 
 dayjs.extend(relativeTime);
 
-export default function Home() {
+export default function Home({ posts }) {
 
-  const [posts, setPosts] = useState<Post[]>([]);
+  // const [posts, setPosts] = useState<Post[]>([]);
 
-  useEffect(() => {
-    Axios.get('/posts')
-      // .then(res => setPosts(res.data))
-      .then(res => setPosts(res.data))
-      .catch(err => console.log(err))
-  }, []) // [] empty dependency array so this only executes ONCE
+  // useEffect(() => {
+  //   Axios.get('/posts')
+  //     // .then(res => setPosts(res.data))
+  //     .then(res => setPosts(res.data))
+  //     .catch(err => console.log(err))
+  // }, []) // [] empty dependency array so this only executes ONCE
 
   return (
     <div className="pt-12">
@@ -93,4 +94,15 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+// SSR - fetches data from the server side via Next.js
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const res = await Axios.get('/posts')
+
+    return { props: { posts: res.data } }
+  } catch (err) {
+    return { props: { error: 'Something went wrong' }}
+  }
 }
